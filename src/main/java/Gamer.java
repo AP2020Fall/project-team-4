686f3fb574e2b7aa9a2dd26abcc6092433455f0a
fatemeh-fo <fotouhi.f20@gmail.com>
2020-11-10 13:16:08 -0800

    added in Gamer class

--- src/main/java/Gamer.java
+++ src/main/java/Gamer.java
@@ -1,11 +1,16 @@
-public class Gamer extends Account{
+public class Gamer extends Account {
 
 	private int score = 0;
 	private int money;
 
 
-	public Gamer (String firstName, String lastName, String username, int money) {
-		super(firstName, lastName, username);
+	public Gamer(String firstName, String lastName, String username, String accountID, String password, String email, String phoneNumber, String daysSinceRegistration, int money, int score, String gamesLog, String friends, String friendsRequestlist,
+				 )
+	{
+		super(firstName, lastName, username,
+				accountID, password, email, phoneNumber, daysSinceRegistration, gamesLog, friends, friendsRequestlist,
+				);
+
 		this.money = money;
 	}
 }
