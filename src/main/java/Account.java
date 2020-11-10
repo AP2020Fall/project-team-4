9c6b3b1b8688a3d7f4469cee503b203a2d815807
fatemeh-fo <fotouhi.f20@gmail.com>
2020-11-10 08:23:00 -0800

    added accountId,  password, email, phone number

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,12 +1,15 @@
-import java.util.ArrayList;
 import java.util.LinkedList;
 
 public abstract class Account {
-	private String firstName, lastName, username;
-	
+	private String firstName, lastName,
+			username,password,
+			accountID,
+			email, phoneNumber ;
+
 	private static LinkedList<Account> accounts = new LinkedList<>();
 
-	public Account (String firstName, String lastName, String username) {
+	public Account(String firstName, String lastName, String username, String accountID, String password, String email, String phoneNumber) {
+
 		// TODO: 11/10/2020 AD
 	}
 
