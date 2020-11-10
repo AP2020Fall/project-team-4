3555e9d649efdbb9e8cff7c760b5124aa6943244
dorrinsotoudeh <dorrinsotudeh123@gmail.com>
2020-11-10 16:23:04 +0400

    added Account.java and Gamer.java

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,3 +1,14 @@
-public class Account {
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public abstract class Account {
+	private String firstName, lastName, username;
+	
+	private static LinkedList<Account> accounts = new LinkedList<>();
+
+	public Account (String firstName, String lastName, String username) {
+		// TODO: 11/10/2020 AD
+	}
+
 
 }
