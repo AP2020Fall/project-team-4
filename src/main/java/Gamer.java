254c0e9ac7128bfcbc111dc4cba1ffddb9637b52
fatemeh-fo <fotouhi.f20@gmail.com>
2020-11-10 14:51:47 -0800

    added event class

--- src/main/java/Gamer.java
+++ src/main/java/Gamer.java
@@ -1,10 +1,12 @@
 public class Gamer extends Account {
-
+    private String firstname, lastname, username, accountId, password, email, phoneNumber, daysSinceRegistration, friends;
+    private Gameslog gameslog;
 	private int score = 0;
 	private int money;
+	private int DaysSinceRegistration = 0;
 
 
-	public Gamer(String firstName, String lastName, String username, String accountID, String password, String email, String phoneNumber, String daysSinceRegistration, int money, int score, String gamesLog, String friends, String friendsRequestlist
+	public Gamer(String firstName, String lastName, String username, String accountID, String password, String email, String phoneNumber, Gameslog gameslog, int daysSinceRegistration, int money, int score, String friends
 				 )
 	{
 		super(firstName, lastName, username,
