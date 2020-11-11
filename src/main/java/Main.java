9e84d70a57fe702e497ab3a9b44a7928bacca3e4
Elahe <elahe1380m@gmail.com>
2020-11-11 20:26:53 +0330

    moved AllGamesmenu and GameMenu methods to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
+
 	// regexes
 
 	private static Menu currentMenu; // FIXME: initialize
@@ -18,6 +19,34 @@ public class Main {
 	public static void main (String[] args) {
 
 	}
+
+
+
+		public static void listGames(){}
+		//lists all games
+		//gets command open <game_name> and calls the method below
+
+		public static void processCommand(String command){}
+		//process command open <game_name> and calls the execution method
+		//change menu if needed
+
+
+	public void scoreboard(String gameName , int gameID){}  //TODO gets logID or gameID as parameter
+
+	public void details(){}
+
+	public void showLog(int gameID , String name){}
+
+	public void showWinsCount(){}  //fixme gets logID or gameID as parameter
+
+	public void showPlayedCount(){}  //fixme gets logID or gameID as parameter
+
+	public void addFavorites(int gameID , int logID){}
+
+	public void runGame(){}   //fixme gets logID or gameID as parameter
+
+	public void showPoints(){}   //fixme gets logID or gameID as parameter
+
 
 	private static Matcher getMatcher (String text, String regex) {
 		return Pattern.compile(regex).matcher(text);
@@ -40,4 +69,5 @@ enum Menu {
 	public static Menu getBack () {
 		return back;
 	}
+
 }
