10d0121fe7d0e31e5a33d2e8c68dddff57c129be
doggooo <dorrinsotoudeh123@gmail.com>
2021-01-04 16:48:31 +0400

    added all the fxml files and their controllers

---
+++ src/com/plato/Controller/Menus/EventPageController.java
@@ -0,0 +1,4 @@
+package Controller.Menus;
+
+public class EventPageController {
+}
---
+++ src/com/plato/Controller/Menus/GamesMenuController.java
@@ -0,0 +1,9 @@
+package Controller.Menus;
+
+public class GamesMenuController {
+	private boolean isForFaveGames;
+
+	public void setIsForFaveGames (boolean forFaveGames) { // todo when fxml is called this method should be called
+		isForFaveGames = forFaveGames;
+	}
+}
