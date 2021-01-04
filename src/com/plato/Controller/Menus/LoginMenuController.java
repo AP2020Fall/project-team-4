fab830919791fe1bbca635935b8e2111868c79e3
doggooo <dorrinsotoudeh123@gmail.com>
2021-01-04 19:58:55 +0400

    login menu stuff added but no code

--- src/com/plato/Controller/Menus/LoginMenuController.java
+++ src/com/plato/Controller/Menus/LoginMenuController.java
@@ -1,11 +1,58 @@
 package Controller.Menus;
 
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class LoginMenuController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginMenuController implements Initializable {
 	private static Stage stage;
+	public StackPane pwStackPane;
+	public ImageView showPwOrNot;
+	public PasswordField pwFieldpwHidden;
+	public TextField pwFieldpwShown;
 
 	public static void setStage (Stage stage) {
 		LoginMenuController.stage = stage;
+	}
+
+	@Override
+	public void initialize (URL url, ResourceBundle resourceBundle) {
+		pwFieldpwShown = (TextField) pwStackPane.getChildren().get(0);
+		pwFieldpwHidden = (PasswordField) pwStackPane.getChildren().get(1);
+		showPwOrNot = (ImageView) pwStackPane.getChildren().get(2);
+
+		Image showImg = new Image("https://cdn1.iconfinder.com/data/icons/essentials-pack/96/show_view_eye_visible_on-256.png"),
+				hideImg = new Image("https://cdn1.iconfinder.com/data/icons/essentials-pack/96/hidden_invisible_hide_eye_private-256.png");
+
+		showPwOrNot.setImage(showImg);
+		pwFieldpwHidden.toFront();
+
+		showPwOrNot.setOnMouseClicked(mouseEvent -> {
+			if (showPwOrNot.getImage().getUrl().equals(showImg.getUrl())) {
+				showPwOrNot.setImage(hideImg);
+				pwFieldpwShown.setText(pwFieldpwHidden.getText());
+				pwFieldpwShown.toFront();
+			}
+			else {
+				showPwOrNot.setImage(showImg);
+				pwFieldpwHidden.setText(pwFieldpwShown.getText());
+				pwFieldpwHidden.toFront();
+			}
+			showPwOrNot.toFront();
+		});
+
+		showPwOrNot.setOnMouseEntered(mouseEvent -> showPwOrNot.setOpacity(0.8));
+		showPwOrNot.setOnMouseExited(mouseEvent -> showPwOrNot.setOpacity(1));
+
+		showPwOrNot.resize(25, 25);
+		showPwOrNot.setSmooth(true);
+		showPwOrNot.toFront();
 	}
 }
