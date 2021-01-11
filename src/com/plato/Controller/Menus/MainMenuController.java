79f964d83bc0a99bfe63d0e3e568e80cddf7d984
elahe <elahe1380m@gmail.com>
2021-01-12 00:00:06 +0330

    /b

--- src/com/plato/Controller/Menus/MainMenuController.java
+++ src/com/plato/Controller/Menus/MainMenuController.java
@@ -18,6 +18,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainMenuController implements Initializable {
+	//b
 	private static boolean gamerOrAdmin;
 	public HBox buttons;
 	public Pane pane;
