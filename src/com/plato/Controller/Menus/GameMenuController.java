9439d586ed2d0480ade6655dec342b7ae1af5587
doggooo <dorrinsotoudeh123@gmail.com>
2021-01-16 00:37:17 +0400

    game menu fxml done

--- src/com/plato/Controller/Menus/GameMenuController.java
+++ src/com/plato/Controller/Menus/GameMenuController.java
@@ -6,18 +6,13 @@ import Controller.MainController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-import javax.swing.text.html.ImageView;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -25,82 +20,49 @@ import java.util.ResourceBundle;
 
 public class GameMenuController implements Initializable {
 	private static String gameName;
-	public GridPane gameInfo;
-	public Label username2Error;
+	private static Stage stage;
 	public Label gameTitle;
-	public ImageView backgroundImage;
 	public TextField username2;
-	public VBox vBox;
-	public Pane pane;
-
-
+	public MenuButton timeLimitMenu;
+	public Label timeLimitAskLabel;
+	public Label username2Error;
+	public GridPane newGamePropertyWindow;
+	public Button addToFaveGamesBtn;
+	public GridPane mainGridPane;
 
 	public static void setStage (Stage stage) {
-		stage.setTitle(gameName);
-		stage.show();
-	}
-
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		gameTitle.setText(gameName);
-		if(gameName.equals("Reversi")){
-			//pane.getChildren().subList(212 , 438).clear();
-		}
-		else if(gameName.equals("BattleSea")){}
+		GameMenuController.stage = stage;
+		GameMenuController.stage.setOnCloseRequest(e -> GameMenuController.stage = null);
 	}
 
 	public static void setGameName (String gameName) {
 		GameMenuController.gameName = gameName;
 	}
 
-	public void startBattleSea () {
-		try {
-			Stage battleSeaStage = MainController.getInstance().createAndReturnNewStage(
-					FXMLLoader.load(new File("src/com/plato/View/Menus/BattleSeaEditBoardPage.fxml").toURI().toURL()),
-					"BattleSea",
-					true,
-					MainController.getInstance().getPrimaryStage()
-			);
-			BattleSeaEditBoardPageController.setStage(battleSeaStage);
-			battleSeaStage.show();
-			//stage.close();
-		} catch (IOException e) {
-			e.printStackTrace();
+	@Override
+	public void initialize (URL location, ResourceBundle resources) {
+		if (gameName.equals("Reversi")) {
+			//pane.getChildren().subList(212 , 438).clear();
 		}
+		else if (gameName.equals("BattleSea")) {}
 	}
-
-	private void startReversi () {
-		try{
-		Stage reversiStage= MainController.getInstance().createAndReturnNewStage(
-				FXMLLoader.load(new File("src/com/plato/View/Menus/ReversiGame.fxml").toURI().toURL()),
-				"Reversi",
-				true,
-				MainController.getInstance().getPrimaryStage()
-		);
-		ReversiGameController.setStage();
-		reversiStage.show();
-		//stage.close();
-	} catch (IOException e){
-			e.printStackTrace();
-		}
-	}
-
 
 	public void newGame (ActionEvent actionEvent) {
-		//gameInfo.setVisible(true);
-		Label title = new Label(gameName);
-		title.setFont(new Font("Bauhaus 93" , 24));
-		VBox vBox = new VBox(title);
-		Scene scene = new Scene(vBox);
-		Stage stage = new Stage();
-		stage.setScene(scene);
-		stage.setTitle("New Game of " + gameName);
-		stage.setWidth(400);
-		stage.setHeight(450);
-		stage.initModality(Modality.WINDOW_MODAL);
-		TextField secondPlayer = new TextField();
-		stage.show();
+//		//gameInfo.setVisible(true);
+//		Label title = new Label(gameName);
+//		title.setFont(new Font("Bauhaus 93", 24));
+//		VBox vBox = new VBox(title);
+//		Scene scene = new Scene(vBox);
+//		Stage stage = new Stage();
+//		stage.setScene(scene);
+//		stage.setTitle("New Game of " + gameName);
+//		stage.setWidth(400);
+//		stage.setHeight(450);
+//		stage.initModality(Modality.WINDOW_MODAL);
+//		TextField secondPlayer = new TextField();
+//		stage.show();
 
+		
 	}
 
 	public void gameInfoGiveDone (ActionEvent actionEvent) {
@@ -116,5 +78,57 @@ public class GameMenuController implements Initializable {
 		}
 	}
 
+	public void startBattleSea () {
+		try {
+			Stage battleSeaStage = MainController.getInstance().createAndReturnNewStage(
+					FXMLLoader.load(new File("src/com/plato/View/Menus/BattleSeaEditBoardPage.fxml").toURI().toURL()),
+					"BattleSea",
+					true,
+					MainController.getInstance().getPrimaryStage()
+			);
+			BattleSeaEditBoardPageController.setStage(battleSeaStage);
+			battleSeaStage.show();
+			stage.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
+	private void startReversi () {
+		try {
+			Stage reversiStage = MainController.getInstance().createAndReturnNewStage(
+					FXMLLoader.load(new File("src/com/plato/View/Menus/ReversiGame.fxml").toURI().toURL()),
+					"Reversi",
+					true,
+					MainController.getInstance().getPrimaryStage()
+			);
+			ReversiGameController.setStage(reversiStage);
+			reversiStage.show();
+			stage.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	public void dontStartGame (ActionEvent actionEvent) {
+	}
+
+	public void startGame (ActionEvent actionEvent) {
+	}
+
+	public void closeGame (ActionEvent actionEvent) {
+	}
+
+	public void addToFaveGames (ActionEvent actionEvent) {
+	}
+
+	public void displayScoreboard (ActionEvent actionEvent) {
+	}
+
+	public void setTime (ActionEvent actionEvent) {
+	}
+
+	public void displayLogOfGame (ActionEvent actionEvent) {
+	}
 }
