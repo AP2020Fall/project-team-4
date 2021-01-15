cb46bfab940bff7800c82c0b12984de903b0c7e0
doggooo <dorrinsotoudeh123@gmail.com>
2021-01-16 00:36:53 +0400

    reversi game fxml done

--- src/com/plato/Controller/Menus/ReversiGameController.java
+++ src/com/plato/Controller/Menus/ReversiGameController.java
@@ -1,52 +1,66 @@
 package Controller.Menus;
 
-import Controller.GameRelated.GameController;
-import Controller.GameRelated.Reversi.ReversiController;
 import Model.GameRelated.Reversi.PlayerReversi;
-import javafx.fxml.Initializable;
 import Model.GameRelated.Reversi.Reversi;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Circle;
-
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ReversiGameController implements Initializable {
+	private static Stage stage;
+	private static Reversi currentGame;
+	public Circle turnIndicatorW;
+	public ImageView pfpW;
+	public ImageView pfpB;
+	public Circle turnIndicatorB;
+	public Label usernameW;
+	public Label usernameB;
+	public Button confirmMoveBtn;
+	private PlayerReversi player1, player2;
 
-    private PlayerReversi player1 , player2;
-    public ImageView pfp2, pfp1;
-    public Label username2, username1;
+	public static void setStage (Stage stage) {
+		ReversiGameController.stage = stage;
+		ReversiGameController.stage.setOnCloseRequest(e -> {
+			ReversiGameController.stage = null;
+			currentGame = null;
+		});
+	}
 
+	public static void setCurrentGame (Reversi currentGame) {
+		ReversiGameController.currentGame = currentGame;
+	}
 
+	@Override
+	public void initialize (URL url, ResourceBundle resourceBundle) {
+		player1 = (PlayerReversi) currentGame.getListOfPlayers().get(0);
+		player2 = (PlayerReversi) currentGame.getListOfPlayers().get(1);
 
+		//setting profile pictures
+		{
+		}
 
+		// setting the usernames
+		{
+		}
+	}
 
-    public static void setStage() {
-    }
+	public void closeGame (ActionEvent actionEvent) {
+		// TODO: 1/15/2021 AD
+	}
 
-    @Override
-    public void initialize (URL url, ResourceBundle resourceBundle) {
-        player1 = (PlayerReversi) GameController.getInstance().getCurrentGameInSession().getListOfPlayers().get(0);
-        player2 = (PlayerReversi) GameController.getInstance().getCurrentGameInSession().getListOfPlayers().get(1);
+	public void putMarkIfPossible (MouseEvent mouseEvent) {
+		// TODO: 1/15/2021 AD
+	}
 
-        //setting profile pictures
-        {
-            pfp1.setImage(new Image(player1.getGamer().getPfpUrl()));
-            pfp1.setSmooth(true);
-            pfp1.setClip(new Circle(pfp1.getBoundsInLocal().getCenterX(), pfp1.getBoundsInLocal().getCenterY(), 50));
-
-            pfp2.setImage(new Image(player2.getGamer().getPfpUrl()));
-            pfp2.setSmooth(true);
-            pfp2.setClip(new Circle(pfp2.getBoundsInLocal().getCenterX(), pfp2.getBoundsInLocal().getCenterY(), 50));
-        }
-
-        // setting the usernames
-        {
-            username1.setText(player1.getUsername());
-            username2.setText(player2.getUsername());
-        }
-    }
+	public void confirmMove (ActionEvent actionEvent) {
+		// TODO: 1/15/2021 AD
+	}
 }
