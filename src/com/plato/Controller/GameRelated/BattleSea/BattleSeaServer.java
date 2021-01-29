3b7a1bf437871ec4c085b64efc9d7ded561ac31e
fatemeh-fo <fotouhi.f20@gmail.com>
2021-01-29 06:04:50 -0800

    changes in main controller

--- src/com/plato/Controller/GameRelated/BattleSea/BattleSeaServer.java
+++ src/com/plato/Controller/GameRelated/BattleSea/BattleSeaServer.java
@@ -1,5 +1,7 @@
 package Controller.GameRelated.BattleSea;
 
+import Model.AccountRelated.Gamer;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -8,14 +10,47 @@ public class BattleSeaServer {
 
     private static ServerSocket serverSocket;
 
-    static class BattleSeaClientHandler{
+    static class BattleSeaClientHandler extends Thread {
+
+
         private Socket clientSocket;
         private  DataInputStream dataInputStream;
         private  DataOutputStream dataOutputStream;
         private BattleSeaServerImp server;
+        private Gamer gamer;
+
+        public BattleSeaClientHandler(Socket clientSocket, DataInputStream dataInputStream, DataOutputStream dataOutputStream, BattleSeaServerImp server) {
+            this.clientSocket = clientSocket;
+            this.dataInputStream = dataInputStream;
+            this.dataOutputStream = dataOutputStream;
+            this.server = server;
+        }
+        @Override
+        public void run()
+        {
+            clientHandler();
 
 
+        }
+        private void clientHandler()
+        {
+            try {
+                String input = "" ;
+                while (true){
+                    input =dataInputStream.readUTF();
+
+                    System.out.println("client sent : "+ input);
+                    if (input.startsWith("")){
+
+                    }
+                }
+
+
+            }
+            catch ()
+        }
     }
+
 
     static class BattleSeaServerImp{
 
@@ -23,15 +58,23 @@ public class BattleSeaServer {
 
         public void runBattleSeaServer(){
             try {
+
                 serverSocket = new ServerSocket(6666);
+
                 clientSocket = serverSocket.accept();
+
                 DataInputStream dataInputStream = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+
                 DataOutputStream dataOutputStream = new DataOutputStream(new BufferedOutputStream(clientSocket.getOutputStream()));
+
+                new BattleSeaClientHandler(clientSocket, dataInputStream, dataOutputStream,this );
+
             } catch (IOException e) {
                 System.out.println("cannot connect to server!");
             }
         }
 
     }
+
 }
 
