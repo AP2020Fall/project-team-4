27f74d652ee4be69efbc23a82f10dc477f300ca7
elahe <elahe1380m@gmail.com>
2021-02-01 19:49:09 +0330

    changes in Server and Client

--- src/com/plato/View/Client.java
+++ src/com/plato/View/Client.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 
 
 public class Client {
+
+
     public static void main (String[] args) throws IOException {
         System.out.println("client started");
         try {
